8a9e62268a6539baa611497fa380166b04ef1f82
arsof-app <arsof.app@gmail.com>
2018-03-03 22:34:41 +0100

    add movilidad-reducida

---
+++ Movilidad-Reducida/app/src/main/java/com/example/ibaitxo/movilidadreducida/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.ibaitxo.movilidadreducida;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Intent intentMaps = new Intent(getApplicationContext(), MapsActivity.class);
+
+        Button verMapa = (Button)findViewById(R.id.verMapa);
+        Button anadirZona = (Button)findViewById(R.id.anadirZona);
+        Button salir = (Button)findViewById(R.id.salir);
+
+        verMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intentMaps);
+            }
+        });
+
+        anadirZona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 0);
+            }
+        });
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                salirApp(view);
+            }
+        });
+
+
+    }
+
+
+    public void salirApp (View view){
+        finish();
+    }
+}
---
+++ Movilidad-Reducida/app/src/main/java/com/example/ibaitxo/movilidadreducida/MapsActivity.java
@@ -0,0 +1,73 @@
+package com.example.ibaitxo.movilidadreducida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class MapsActivity extends AppCompatActivity {
+
+    private MapView mapView = null;
+    private MapboxMap mapboxMap = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapboxAccountManager.start(this, getString(R.string.access_token));
+        setContentView(R.layout.activity_maps);
+
+        mapView = (MapView)findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap map) {
+                mapboxMap = map;
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(48.85819, 2.29458))
+                        .title("Torre Eiffel")
+                );
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(42.805409, -1.679583))
+                        .title("Mi casa")
+                );
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+}
