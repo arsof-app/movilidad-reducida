b8a9de71766a09ec2e9c4800ec3eb5e8b0f47755
arsof-app <arsof.app@gmail.com>
2018-03-15 12:04:42 +0100

    15-03-18_1204

--- Movilidad-Reducida/app/src/main/java/com/example/ibaitxo/movilidadreducida/CameraActivity.java
+++ Movilidad-Reducida/app/src/main/java/com/example/ibaitxo/movilidadreducida/CameraActivity.java
@@ -1,12 +1,27 @@
 package com.example.ibaitxo.movilidadreducida;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import java.util.Date;
+import java.io.File;
+import java.text.SimpleDateFormat;
 
 import com.example.ibaitxo.movilidadreducida.modelo.CameraPreview;
 
@@ -15,31 +30,63 @@ import com.example.ibaitxo.movilidadreducida.modelo.CameraPreview;
  */
 
 public class CameraActivity extends AppCompatActivity{
-    private Bitmap image;
-    private Camera mCamera;
-    private CameraPreview mPreview;
+    private ImageView imageView;
+    private Button takePictureButton;
+    private Uri file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        Camera cam = getCameraInstance();
+        takePictureButton = (Button) findViewById(R.id.button_capture);
+        imageView = (ImageView) findViewById(R.id.camera_preview);
 
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
     }
 
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK); // attempt to get a Camera instance
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                takePictureButton.setEnabled(true);
+            }
         }
-        catch (Exception e) {
-            Log.v("Camera error: " + e.getMessage(), "getCameraInstance");
+    }
+
+    public void takePicture(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(file);
+            }
         }
-        return c; // returns null if camera is unavailable
+    }
+
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
     }
 }
