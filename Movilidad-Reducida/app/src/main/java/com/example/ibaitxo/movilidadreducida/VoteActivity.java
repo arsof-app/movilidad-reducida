dde9635090c6f0c08c41d250ab9688765e29e956
Leralite <joshlera444@gmail.com>
2018-04-30 19:22:12 +0200

    rehecho activity_vote.xml

--- Movilidad-Reducida/app/src/main/java/com/example/ibaitxo/movilidadreducida/VoteActivity.java
+++ Movilidad-Reducida/app/src/main/java/com/example/ibaitxo/movilidadreducida/VoteActivity.java
@@ -37,6 +37,7 @@ public class VoteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_vote);
 
         Intent intent = getIntent();
